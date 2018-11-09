44c98c2d452bd08bf6620c0aab621db6bd654264
sarzynski <kamil.s.sarzynski@gmail.com>
2018-11-09 15:02:09 +0100

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 before_action :correct_user,   only: [:edit, :update]
 before_action :admin_user, only: [:destroy]
 
-def index
+  def index
     @users = User.paginate(page: params[:page])
   end
 
@@ -16,15 +16,15 @@ def index
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-end
-end
+      @user = User.new(user_params)
+      if @user.save
+        @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
+      else
+        render 'new'
+      end
+    end
 
 def edit
   @user = User.find(params[:id])
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,18 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-
-  test "valid signup information" do
-     get signup_path
-     assert_difference 'User.count', 1 do
-       post users_path, params: { user: { name:  "Example User",
-                                          email: "user@example.com",
-                                          password:              "password",
-                                          password_confirmation: "password" } }
-     end
-     follow_redirect!
-     assert_template 'users/show'
-     assert is_logged_in?
-   end
- end
+end
